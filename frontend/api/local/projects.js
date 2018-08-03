fdcd2a9c118a649f595facc5b147f55a6d5ccc30
Anna Holland-Smith <anna.hollandsmith3@gmail.com>
2018-08-03 22:51:22 +0100

    update to projects

--- frontend/api/local/projects.js
+++ frontend/api/local/projects.js
@@ -3,92 +3,24 @@
 const projects = [
 	{
 		id: 0,
-		name: 'Belleza Organica',
-		giturl: 'https://github.com/Phaze1D/BellezaNodeJS',
-		liveurl: 'https://bellezaorganica.com.mx',
-		description: 'Production Grade e-commerce website built for a client.',
-		topics: ['ReactJS', 'Redux', 'ExpressJS', 'Sequelize', 'AWS'],
-		languages: ['Javascript','HTML','SASS'],
+		name: 'MakersBnB',
+		giturl: 'hhttps://github.com/AnnaHollandSmith/MakersBnB',
+		liveurl: 'https://taark-bnb.herokuapp.com/',
+		description: 'Our first ever group project during Makers Academy, a coding bootcamp based in London. The site is built in Ruby and was created in a group through the use of pair-programming and effective git wokflows',
+		topics: ['Ruby', 'Heroku', 'Sinatra'],
+		languages: ['Ruby','HTML','CSS', 'Capybara', 'Rspec'],
 		images: [require('resources/images/bo1.jpg'), require('resources/images/bo2.jpg'), require('resources/images/bo3.jpg')]
 	},
 	{
 		id: 1,
-		name: 'Google Zomato Maps',
-		giturl: 'https://github.com/Phaze1D/Google_Zomato_Maps',
-		liveurl: 'https://phaze1d.github.io/Google_Zomato_Maps/',
-		description: 'Google Maps Replica that also uses Zomato\'s API to search for restaurants',
-		topics: ['KnockoutJS', 'Google Maps API', 'Zomato API'],
-		languages: ['Javascript', 'HTML', 'CSS'],
-		images: [require('resources/images/gzm1.jpg'), require('resources/images/gzm2.jpg'), require('resources/images/gzm3.jpg')]
-	},
-	{
-		id: 2,
-		name: 'Blug',
-		giturl: 'https://github.com/Phaze1D/Blug',
-		liveurl: 'https://blog-160318.appspot.com/',
-		description: 'A simple user blog that allows users to post their stories. Kind of like Reddit but simpler',
-		topics: ['ReactJS', 'Redux', 'Flask', 'Google Cloud Platform'],
-		languages: ['Python', 'Javascript', 'SASS'],
-		images: [require('resources/images/bl1.jpg'), require('resources/images/bl2.jpg'), require('resources/images/bl3.jpg')]
-	},
-	{
-		id: 3,
-		name: 'Popdris',
-		giturl: 'https://github.com/Phaze1D/Popdris',
-		liveurl: 'https://phaze1d.github.io/Popdris/',
-		description: 'A simple game that mixes Candy Crush and Tetris gameplay',
-		topics: ['PixiJS', 'HTML Canvas', 'OOP'],
-		languages: ['Javascript', 'HTML'],
-		images: [require('resources/images/po1.jpg'), require('resources/images/po2.jpg')]
-	},
-	{
-		id: 4,
-		name: 'Interactive Resume',
-		giturl: 'https://github.com/Phaze1D/Interactive-Resume',
-		liveurl: 'https://david-villarreal.com',
-		description: 'This is my Interactive Resume that you are currently viewing right now.',
-		topics: ['ReactJS', 'Redux', 'Firebase'],
-		languages: ['Javascript', 'SASS'],
-		images: [require('resources/images/ir1.jpg'), require('resources/images/ir2.jpg'), require('resources/images/ir3.jpg')]
-	},
-	{
-		id: 5,
-		name: 'Farmage Frontend',
-		giturl: 'https://github.com/Phaze1D/Farmage-Frontend',
-		description: 'The is a farm management application that simplifies the process of maintaining and organizing financial data of any farm. A project that I wish to finish some day.',
-		topics: ['ReactJS', 'MeteorJS', 'MongoDB'],
-		languages: ['Javascript', 'Less', 'HTML'],
-		images: [require('resources/images/ff1.jpg'), require('resources/images/ff2.jpg'), require('resources/images/ff3.jpg')]
-	},
-	{
-		id: 6,
-		name: 'Farmage Concept',
-		giturl: 'https://github.com/Phaze1D/Farmage-Concept',
-		description: 'This is the concept version of my farm management app. This version of the app is mostly focused on the backend',
-		topics: ['MeteorJS','MongoDB', 'Blaze Components'],
-		languages: ['CoffeeScript', 'HTML', 'Less'],
-		images: [require('resources/images/fc1.jpg'), require('resources/images/fc2.jpg'), require('resources/images/fc3.jpg')]
-	},
-	{
-		id: 7,
-		name: 'Rising Fall Android',
-		giturl: 'https://github.com/Phaze1D/RisingFallAndroidV2',
-		liveurl: 'https://play.google.com/store/apps/details?id=com.Phaze1D.RisingFallAndroidV2.android&hl=en',
-		description: 'This is the first game that I built for Android. A game mix between Candy Crush and Tetris',
-		topics: ['Libgdx', 'Android'],
-		languages: ['Java'],
-		images: [require('resources/images/rf1.jpg'), require('resources/images/rf2.jpg'), require('resources/images/rf3.jpg')]
-	},
-	{
-		id: 8,
-		name: 'Rising Fall IOS',
-		giturl: 'https://github.com/Phaze1D/RisingFallIOS',
-		liveurl: 'https://itunes.apple.com/za/app/rising-fall/id904055570?mt=8',
-		description: 'This is the first game that I built for IOS. A game mix between Candy Crush and Tetris',
-		topics: ['Spritekit', 'IOS'],
-		languages: ['Objective C'],
-		images: [require('resources/images/rf1.jpg'), require('resources/images/rf2.jpg'), require('resources/images/rf3.jpg')]
-	},
+		name: 'Pear',
+		giturl: 'https://github.com/AnnaHollandSmith/pear',
+		liveurl: 'http://findapear.herokuapp.com/users/sign_in',
+		description: 'A web-based application designed to enable developers (amateur and professional) to find like-minded individuals to pair program with (locally or remotely).',
+		topics: ['Ruby on Rails', 'Javascript', 'Rspec', 'Capybara', 'Jasmine'],
+		languages: ['Ruby', 'JavaScript'],
+		images: [require('resources/images/bo1.jpg'), require('resources/images/bo2.jpg'), require('resources/images/bo3.jpg')]
+	}
 ]
 
 function search(query){
