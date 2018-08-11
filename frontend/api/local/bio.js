da8b766d6c9e6217ce778428002d414cf729b839
Anna Holland-Smith <anna.hollandsmith3@gmail.com>
2018-08-11 15:36:40 +0100

    add content to bio

--- frontend/api/local/bio.js
+++ frontend/api/local/bio.js
@@ -10,7 +10,14 @@ const bio = {
 	},
 
 	about: `
-	I am a former criminal defence lawyer, turned software engineer 👩‍💻 
+	I am a former criminal defence lawyer, turned software engineer 👩‍💻 \n
+	I wrote my first line of code with the non-profit organisation CodeFirst: Girls, who provide free introductory coding courses for women across the UK. After a couple of months of independent study, I attended Makers Academy, a coding bootcamp based in London that teaches a Ruby stack. Upon completion of the course, and approximately six months after writing that very first line of code, I started work as a Software Engineer at the BBC. That same month, I returned to CodeFirst: Girls as a volunteer instructor. \n
+
+	Having successfully transitioned into a new career, I remain committed to encouraging and enabling others to do the same through the provision of a more accessible coding education. I am proud to be one of the organisers behind CodeYourFuture Manchester - we are a charity, teaching refugees and asylum seekers how to code and preparing them for Junior Developer roles across the North West of England. \n
+
+	As a career changer without a traditional computer science background, I am committed to democratising coding education and promoting a more diverse, inclusive and equitable Tech workforce. I am an instructor for CodeFirst:Girls in Manchester, a non-profit seeking to redress the gender imbalance in Tech by providing free coding courses to ambitious young women from a non-computer science background. I am also one of the organisers behind Codebar Manchester, providing monthly coding workshops for women and members of the LGBTQ+ community. \n
+
+	Having worked closely with the prison and criminal justice systems and witnessed the problematic cycle of reoffending in my previous career in criminal defence law, I was keen to find a way to replicate the work being done by The Last Mile, a scheme in California that teaches prison inmates how to code. I was happy to come on board as a volunteer during the early stages of Code4000, a UK non-profit teaching prisoners how to code so that they can find jobs when they leave, making them far less likely to reoffend. Code4000 is currently running a pilot for 32 prisoners at HMP Humber.
 	`,
 }
 
